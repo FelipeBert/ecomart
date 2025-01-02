066948bc8c2d5add1f038abfedb58daad87723ff
Felipe Bertulino <117488841+FelipeBert@users.noreply.github.com>
2025-01-02 14:19:19 -0300

    refactor: update CategorizadorDeProdutosController to use GPT-4o-mini and token counting with jtokkit
    - Updated the CategorizadorDeProdutosController class to use the gpt-4o-mini model for improved AI responses.
    - Introduced a private method to centralize OpenAI API interactions and ensure cleaner code.

--- src/main/java/org/FelipeBert/ecomart/controller/CategorizadorDeProdutosController.java
+++ src/main/java/org/FelipeBert/ecomart/controller/CategorizadorDeProdutosController.java
@@ -1,6 +1,9 @@
 package org.FelipeBert.ecomart.controller;
 
+import com.knuddels.jtokkit.Encodings;
+import com.knuddels.jtokkit.api.ModelType;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.prompt.ChatOptionsBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +16,11 @@ public class CategorizadorDeProdutosController {
     private final ChatClient chatClient;
 
     public CategorizadorDeProdutosController(ChatClient.Builder chatClientBuilder) {
-        this.chatClient = chatClientBuilder.build();
+        this.chatClient = chatClientBuilder
+                .defaultOptions(ChatOptionsBuilder.builder()
+                        .withModel("gpt-4o-mini")
+                        .build())
+                .build();
     }
 
     @GetMapping
@@ -34,13 +41,23 @@ public class CategorizadorDeProdutosController {
             Resposta: Esportes
             """;
 
+        var tokens = contarTokens(system, produto);
+        System.out.println("Quantidade de Tokens: " + tokens);
+
         return this.chatClient.prompt()
                 .system(system)
                 .user(produto)
                 .options(ChatOptionsBuilder.builder()
                         .withTemperature(0.85)
                         .build())
+                .advisors(new SimpleLoggerAdvisor())
                 .call()
                 .content();
+    }
+
+    private int contarTokens(String system, String user){
+        var registry = Encodings.newDefaultEncodingRegistry();
+        var enc = registry.getEncodingForModel(ModelType.GPT_4O_MINI);
+        return enc.countTokens(system + user);
     }
 }
